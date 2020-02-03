4cd513ce4113fd36818c2aca8358c3f95a534967
mawanda henry <naco123@bitbucket.org>
2020-02-03 22:54:00 +0300

    modifications to emailing

--- mail.php
+++ mail.php
@@ -9,9 +9,7 @@
 	
 	$name = $_POST['name'];
 	$email = $_POST['email'];
-	$phone = $_POST['phone'];
-	$website = $_POST['website'];
-	$message = $_POST['message'];
+	$comment = $_POST['message'];
 	
 	if( empty($name) ){
 		$data['error'] = 'Please enter your name.';
@@ -19,28 +17,48 @@
 		$data['error'] = 'Please enter a valid email address.';
 	}else if( empty($message) ){
 		$data['error'] = 'The message field is required!';
-	}else if( empty($phone) ){
-		$data['error'] = 'Please enter your phone number.';
-	}else if ( empty($website) ){
-		$data['error'] = 'Please enter your website.';
 	}else{
 		
-		$formcontent="From: $name\nPhone: $phone\nWebsite: $website\nEmail: $email\nMessage: $message";
+		// $formcontent="From: $name\nPhone: $phone\nWebsite: $website\nEmail: $email\nMessage: $message";
 		
 		
-		//Place your Email Here
-		$recipient = "mawanda111@gmail.com";
+		// //Place your Email Here
+		// $recipient = "mawanda111@gmail.com";
 		
-		$mailheader = "From: $email \r\n";
+		// $mailheader = "From: $email \r\n";
 		
-		if( mail($recipient, $name, $formcontent, $mailheader) == false ){
-			$data['error'] = 'Sorry, an error occured!';
-		}else{
-			$data['error'] = false;
-		}
+		// if( mail($recipient, $name, $formcontent, $mailheader) == false ){
+		// 	$data['error'] = 'Sorry, an error occured!';
+		// }else{
+		// 	$data['error'] = false;
+		// }
+		$to = 'mawanda111@gmail.com';	
+	//sender - from the form
+	$from = $name . ' <' . $email . '>';
+	
+	//subject and the html message
+	$subject = 'Message from ' . $name;	
+	$message = 'Name: ' . $name . '<br/><br/>
+		       Email: ' . $email . '<br/><br/>		
+		       Message: ' . nl2br($comment) . '<br/>';
+
+	//send the mail
+	$result = sendmail($to, $subject, $message, $from);
+	
 	
 	}
+
+	function sendmail($to, $subject, $message, $from) {
+		$headers = "MIME-Version: 1.0" . "\r\n";
+		$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
+		$headers .= 'From: ' . $from . "\r\n";
+		
+		$result = mail($to,$subject,$message,$headers);
+		
+		if ($result) return 1;
+		else return 0;
+	}
 	
-	echo json_encode($data);
+
 	
 ?>
